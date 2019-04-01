fc988abb29d8db6db4721026df767cf58977656a
Irving Lopez <ilopezp@gbm.com.mx>
2019-04-01 14:30:06 -0600

    - Init commit: Hello world app created by the IDE tools

---
+++ app/src/main/java/mx/com/irvinglop/yahoostock/MainActivity.kt
@@ -0,0 +1,12 @@
+package mx.com.irvinglop.yahoostock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
