9e222633edd6a2a1b3f3c831fdcc7caef754f648
Irving Lopez <ilopezp@gbm.com.mx>
2019-04-01 19:57:24 -0600

    - Hello world Rx

--- app/src/main/java/mx/com/irvinglop/yahoostock/MainActivity.kt
+++ app/src/main/java/mx/com/irvinglop/yahoostock/MainActivity.kt
@@ -2,11 +2,22 @@ package mx.com.irvinglop.yahoostock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.Observable
+import io.reactivex.functions.Consumer
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Observable.just("Hello Please Use this app responsibly!")
+                .subscribe(object : Consumer<String> {
+                    override fun accept(text: String?) {
+                        welcomeTextView.text = text
+                    }
+                }
+                )
     }
 }
