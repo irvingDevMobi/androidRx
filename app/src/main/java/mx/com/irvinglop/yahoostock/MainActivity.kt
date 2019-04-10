70e50c40e22c800d673da3f9985bcc3bc89a8da0
Irving Lopez <ilopezp@gbm.com.mx>
2019-04-10 18:54:37 -0500

    - Schedule examples: test to know in what thread a process run

--- app/src/main/java/mx/com/irvinglop/yahoostock/MainActivity.kt
+++ app/src/main/java/mx/com/irvinglop/yahoostock/MainActivity.kt
@@ -1,9 +1,12 @@
 package mx.com.irvinglop.yahoostock
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import mx.com.irvinglop.yahoostock.entity.StockUpdate
 import java.math.BigDecimal
@@ -29,5 +32,38 @@ class MainActivity : AppCompatActivity() {
                 StockUpdate("Apple", BigDecimal(645.1), Date()),
                 StockUpdate("Twitter", BigDecimal(1.43), Date()))
                 .subscribe { text -> stockDataAdapter.add(text) }
+        Log.e(TAG, "*******************************")
+        Observable.just("firstItem", "SecondItem")
+                .doOnNext { Log.d(TAG, "onNext: $it Thread:" + Thread.currentThread().name) }
+                .subscribe {
+                    Log.d(TAG, "subscribe: $it Thread" + Thread.currentThread().name)
+                    Log.e(TAG, "*******************************")
+                }
+        Observable.just("firstItem2", "SecondItem2")
+                .subscribeOn(Schedulers.io())
+                .doOnNext { Log.d(TAG, "onNext: $it Thread:" + Thread.currentThread().name) }
+                .subscribe {
+                    Log.d(TAG, "subscribe: $it Thread" + Thread.currentThread().name)
+                    Log.e(TAG, "*******************************")
+                }
+        Observable.just("firstItem3", "SecondItem3")
+                .subscribeOn(Schedulers.io())
+                .doOnNext { Log.d(TAG, "onNext: $it Thread:" + Thread.currentThread().name) }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    Log.d(TAG, "subscribe: $it Thread" + Thread.currentThread().name)
+                    Log.e(TAG, "*******************************")
+                }
+        Observable.just("firstItem4", "SecondItem4")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext { Log.d(TAG, "onNext: $it Thread:" + Thread.currentThread().name) }
+                .subscribe {
+                    Log.d(TAG, "subscribe: $it Thread" + Thread.currentThread().name)
+                }
+    }
+
+    companion object {
+        const val TAG = "YahooApp"
     }
 }
